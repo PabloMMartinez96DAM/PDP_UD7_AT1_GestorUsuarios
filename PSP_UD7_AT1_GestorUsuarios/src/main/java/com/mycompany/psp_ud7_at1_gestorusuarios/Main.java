17a065baeb246d44da55f4cb45efa9799c732288
Pablo <699328@alu.murciaeduca.es>
2023-02-08 20:14:37 +0100

    UD7 EJ1 PSP

---
+++ PSP_UD7_AT1_GestorUsuarios/src/main/java/com/mycompany/psp_ud7_at1_gestorusuarios/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.psp_ud7_at1_gestorusuarios;
+
+/**
+ *
+ * @author dam
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
