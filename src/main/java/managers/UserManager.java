8907e6c5f623f5268fd5097f7df45a3e554fbdfa
Pablo <699328@alu.murciaeduca.es>
2023-02-08 20:52:36 +0100

    Login implementado

--- src/main/java/managers/UserManager.java
+++ src/main/java/managers/UserManager.java
@@ -4,12 +4,38 @@
  */
 package managers;
 
+import java.util.ArrayList;
+import java.util.List;
+import models.UserModel;
+
 /**
  *
  * @author dam
  */
 public class UserManager {
+
+    private List<UserModel> users = new ArrayList<UserModel>();
+
     
-    
-    
+    public boolean login(UserModel user) {
+        boolean isFound = false;
+
+        //Recorremos la lista de usuarios para comparar si el usuario que intenta
+        //Loguearse existe en la lista de usuarios
+        for (int i = 0; i < users.size() && !isFound; i++) {
+
+            // Comprobamos si el nombre coincide para el log
+            if (user.getName().equals(users.get(i).getName())) {
+
+                //TODO: Logs bonitos
+                // Si el usuario es igual, comprobamos la contraseña
+                if (user.getPass().equals(users.get(i).getPass())) {
+
+                    System.out.println("El usuario:" + user.getName() + " se ha logueado correctamente");
+                    isFound = true;
+                }
+            }
+        }
+        return isFound;
+    }
 }
--- src/main/java/models/UserModel.java
+++ src/main/java/models/UserModel.java
@@ -9,5 +9,45 @@ package models;
  * @author dam
  */
 public class UserModel {
-    
+
+    // Propiedades
+    private int id;
+    private String name;
+    private String pass;
+
+    // Contructores
+    public UserModel() {
+    }
+
+    public UserModel(int id, String name, String pass) {
+        this.id = id;
+        this.name = name;
+        this.pass = pass;
+    }
+
+    // Getters & Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
 }
